9afedcb6abcccabcabb1c86a239ee16a7ac8274e
Josho Hack <josho2hack@gmail.com>
2019-08-16 15:12:40 +0700

    Add project files.

---
+++ Point01Service/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Point01Service
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer(); // name space(using System.Timers;)  
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer.Interval = 5000; //number in milisecinds  
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
