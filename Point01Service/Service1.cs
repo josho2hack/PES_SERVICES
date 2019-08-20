355539383ffcf73d6a809db6b0132b63a4506adf
Josho Hack <josho2hack@gmail.com>
2019-08-20 18:51:05 +0700

    Update

--- Point01Service/Service1.cs
+++ Point01Service/Service1.cs
@@ -4,35 +4,74 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using Newtonsoft.Json;
 
 namespace Point01Service
 {
     public partial class Service1 : ServiceBase
     {
+        private readonly EPESEntities _context;
+
         Timer timer = new Timer(); // name space(using System.Timers;)  
-        public Service1()
+        public Service1(EPESEntities context)
         {
             InitializeComponent();
+            _context = context;
         }
 
         protected override void OnStart(string[] args)
         {
             timer.Elapsed += OnTimedEvent;
-            timer.Interval = 60000; //number in milisecinds  60000 = 1 Min.
+            timer.Interval = 86400000; //(24 Hour) number in milisecinds  60000 = 1 Min.
             timer.Enabled = true;
-        }
-
-        private void OnTimedEvent(object sender, ElapsedEventArgs e)
-        {
-            throw new NotImplementedException();
         }
 
         protected override void OnStop()
         {
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            if (DateTime.Now.Day == 15)
+            {
+                GetDataPoint01();
+            }
+        }
+
+        private void GetDataPoint01()
+        {
+            var office = _context.Offices.Where(d => d.Code != "00000000" && d.Code.Substring(5, 3) == "000");
+            foreach (var item in office)
+            {
+                var url = "http://10.20.37.11:7072/serviceTier/webapi/All/officeId/" + item.Code + "/year/" + DateYear.ToString("D4") + "/month/" + DateTime.Now.AddMonths(-1).Month.ToString("D2");
+                var tax = _download_serialized_json_data<RootObject>(url);
+                var taxOwn = tax.TaxCol.FirstOrDefault(t => t.officeCode == item.Code);
+                if (taxOwn != null)
+                {
+                    or.ResultMonthValue = taxOwn.CMcurrentYear;
+                }
+            }
+        }
+
+        private static T _download_serialized_json_data<T>(string url) where T : new()
+        {
+            using (var w = new WebClient())
+            {
+                var json_data = string.Empty;
+                // attempt to download JSON data as a string
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch (Exception) { }
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
         }
     }
 }
---
+++ Point01Service/TaxCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point01Service
+{
+    public class RootObject
+    {
+        public TaxCollection[] TaxCol { get; set; }
+    }
+
+    public class TaxCollection
+    {
+        public decimal CMCYforcast { get; set; }
+        public decimal CMcurrentYear { get; set; }
+
+        public string officeCode { get; set; }
+    }
+}
